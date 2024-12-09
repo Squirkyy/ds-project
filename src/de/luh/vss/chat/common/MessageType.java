0793529b518cf8ab95922fb6f423b00ed83a3088
Darius <dariusv@outlook.de>
2024-12-09 23:02:25 +0100

    Added JavaDoc to MessageType.java

--- src/de/luh/vss/chat/common/MessageType.java
+++ src/de/luh/vss/chat/common/MessageType.java
@@ -5,47 +5,95 @@ import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Enum representing the different types of messages that can be exchanged in the chat system.
+ */
 public enum MessageType {
-	ERROR_RESPONSE(0, Message.ErrorResponse.class), REGISTER_REQUEST(1, Message.RegisterRequest.class),
-	REGISTER_RESPONSE(2, Message.RegisterResponse.class), CHAT_MESSAGE(4, Message.ChatMessage.class);
+        /**
+         * Error response message type.
+         */
+    	ERROR_RESPONSE(0, Message.ErrorResponse.class),
 
-	private final int msgType;
+        /**
+         * Register request message type.
+         */
+	REGISTER_REQUEST(1, Message.RegisterRequest.class),
 
-	private Constructor<? extends Message> constr;
+        /**
+         * Register response message type.
+         */
+        REGISTER_RESPONSE(2, Message.RegisterResponse.class),
 
-	private static final Map<Integer, MessageType> lookup = new HashMap<Integer, MessageType>();
-	static {
-		for (final MessageType mt : MessageType.values()) {
-			lookup.put(mt.msgType, mt);
-		}
-	}
+        /**
+         * Chat message type.
+         */
+        CHAT_MESSAGE(4, Message.ChatMessage.class);
 
-	private MessageType(int msgType, final Class<? extends Message> cls) {
-		this.msgType = msgType;
-		try {
-			this.constr = cls.getConstructor(DataInputStream.class);
-		} catch (NoSuchMethodException | SecurityException e) {
-			System.err.println("Error while registering message type. "
-					+ "Constructor from DataInputStream not present or accessible");
-			e.printStackTrace();
-			System.exit(-1);
-		}
-	}
+    private final int msgType;
 
-	int msgType() {
-		return msgType;
-	}
+    private Constructor << ? extends Message > constr;
 
-	public static MessageType fromInt(final int val) {
-		return lookup.get(val);
-	}
+    /**
+     * A lookup table to map message type integers to their corresponding MessageType instances.
+     */
+    private static final Map < Integer, MessageType > lookup = new HashMap < Integer, MessageType > ();
+    static {
+        for (final MessageType mt: MessageType.values()) {
+            lookup.put(mt.msgType, mt);
+        }
+    }
 
-	public static Message fromInt(final int val, final DataInputStream in) throws ReflectiveOperationException {
-		final MessageType mt = fromInt(val);
-		if (mt == null) {
-			throw new IllegalStateException("Unknown message type " + val);
-		}
-		return mt.constr.newInstance(in);
-	}
+    /**
+     * Constructs a MessageType instance.
+     * 
+     * @param msgType the integer representation of the message type
+     * @param cls     the class corresponding to this message type
+     */
+    private MessageType(int msgType, final Class << ? extends Message > cls) {
+        this.msgType = msgType;
+        try {
+            this.constr = cls.getConstructor(DataInputStream.class);
+        } catch (NoSuchMethodException | SecurityException e) {
+            System.err.println("Error while registering message type. " +
+                "Constructor from DataInputStream not present or accessible");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    /**
+     * Returns the integer representation of the message type.
+     * 
+     * @return the integer value of the message type
+     */
+    int msgType() {
+        return msgType;
+    }
+
+    /**
+     * Retrieves the MessageType instance corresponding to a given integer value.
+     * 
+     * @param val the integer value representing a message type
+     * @return the corresponding MessageType instance, or null if no match is found
+     */
+    public static MessageType fromInt(final int val) {
+        return lookup.get(val);
+    }
+
+    /**
+     * Parses a message from its integer type and input stream.
+     * 
+     * @param val the integer representation of the message type
+     * @param in  the input stream containing serialized message data
+     * @return the parsed Message instance
+     * @throws ReflectiveOperationException if an error occurs during message instantiation
+     */
+    public static Message fromInt(final int val, final DataInputStream in ) throws ReflectiveOperationException {
+        final MessageType mt = fromInt(val);
+        if (mt == null) {
+            throw new IllegalStateException("Unknown message type " + val);
+        }
+        return mt.constr.newInstance( in );
+    }
 
 }
